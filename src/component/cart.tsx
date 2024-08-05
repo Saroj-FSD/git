8c37ae91d66565e5c1d4c2a8a769d28faa859f5f
Saroj-FSD <sarojpandey602@gmail.com>
2024-08-05 16:20:35 +0545

    look it

--- src/component/cart.tsx
+++ src/component/cart.tsx
@@ -8,7 +8,7 @@ const Cart = (props: ICart)=>{
     return(
         <div>
             <h1 className="bg-red-600 text-white">{props.name}</h1>
-            <p className="text-yellow-950">{props.address}</p>
+            <p className="text-yellow-800">{props.address}</p>
         </div>
     );
 }
